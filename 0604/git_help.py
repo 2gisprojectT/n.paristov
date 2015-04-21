ea6614a9ccd69d462ff46813eeffa3f998d5539d
Lokinay <whitefox.nsk@gmail.com>
2015-04-21 20:55:56 +0600

    Исправил на тесты для гита

---
+++ 0604/git_help.py
@@ -0,0 +1,15 @@
+__author__ = 'whitefoxnsk'
+
+from base_component import BaseComponent
+from selenium.webdriver.common.keys import Keys
+
+
+class git_help(BaseComponent):
+
+    selectors = {
+        'self': '/html/body/div[1]/div[1]/div',
+        'click': '/html/body/div[1]/div[1]/div/ul[1]/li[4]/a',
+    }
+
+    def help(self):
+        self.driver.find_element_by_xpath(self.selectors['click']).click()
